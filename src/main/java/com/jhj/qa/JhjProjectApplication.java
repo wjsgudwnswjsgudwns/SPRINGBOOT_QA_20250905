d8c2518fa9701f749d21d9624fd9fb6bc59578a5
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-09-05 15:17:59 +0900

    application.properties H2 데이터베이스 설정

---
+++ src/main/java/com/jhj/qa/JhjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jhj.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhjProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhjProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhj/qa/JhjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhj.qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhjProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
