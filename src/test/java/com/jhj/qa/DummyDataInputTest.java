738d73b9e36f3fb86343226976f35bae027b7e36
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-09-09 17:07:50 +0900

    더미 null 값 추가 수정

--- src/test/java/com/jhj/qa/DummyDataInputTest.java
+++ src/test/java/com/jhj/qa/DummyDataInputTest.java
@@ -17,7 +17,7 @@ public class DummyDataInputTest {
 		for(int i=1;i<=300;i++) {
 			String subject = String.format("테스트 데이터입니다:[%03d]",i);
 			String content = "연습 내용 더미데이터 입니다.";
-			questionService.create(subject, content);
+			questionService.create(subject, content, null);
 		}
 	}
 }
